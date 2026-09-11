99b72c51ffe442d5a66ea9b48553732912ca389a
Jammy555 <prathap.venkata@gmail.com>
2026-09-11 11:30:23 +0530

    switch
    Change-Id: I9f79a37e7244c14d977548cd908c2d715d48a3fb

--- scripts/volt.sh
+++ scripts/volt.sh
@@ -51,12 +51,12 @@ SYNC_HISTORY_FILE=".repo/.volt_sync_history"
 
 # --- Tree lookup: "repo_url|local_path|display_name|default_branch" --------
 declare -A TREE_LOOKUP=(
-    [kernel]="https://github.com/Jammy555/android_kernel_oneplus_sm8350.git|./kernel/oneplus/sm8350|kernel|Sakura"
+    [kernel]="https://github.com/Jammy555/android_kernel_oneplus_sm8350.git|./kernel/oneplus/sm8350|kernel|16.2"
     [device]="https://github.com/Jammy555/android_device_oneplus_lemonade.git|./device/oneplus/lemonade|device tree|LUN"
-    [common]="https://github.com/Jammy555/android_device_oneplus_sm8350-common.git|./device/oneplus/sm8350-common|common tree|Sakura"
-    [hardware]="https://github.com/Jammy555/hardware_oplus.git|./hardware/oplus|hardware|Sakura"
+    [common]="https://github.com/Jammy555/android_device_oneplus_sm8350-common.git|./device/oneplus/sm8350-common|common tree|16.2"
+    [hardware]="https://github.com/Jammy555/hardware_oplus.git|./hardware/oplus|hardware|16.2"
     [vendor]="https://github.com/Jammy555/vendor_oneplus_lemonade.git|./vendor/oneplus/lemonade|vendor lemonade|VOS-t"
-    [vendor-common]="https://github.com/Jammy555/vendor_oneplus_sm8350-common.git|./vendor/oneplus/sm8350-common|vendor common|Sakura"
+    [vendor-common]="https://github.com/Jammy555/vendor_oneplus_sm8350-common.git|./vendor/oneplus/sm8350-common|vendor common|Lun"
     [camera]="https://github.com/Jammy555/vendor_oplus_camera.git|./vendor/oplus/camera|oplus camera|16"
     [dolby]="https://github.com/Jammy555/vendor_oneplus_dolby.git|./vendor/sony/dolby|dolby|D2"
     [pixelworks]="https://github.com/LineageOS/android_hardware_pixelworks_interfaces.git|hardware/pixelworks/interfaces|pixelworks|lineage-23.2"
